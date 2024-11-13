acc153159d1f25784130af5132fed54b7983844e
Nicholas Ly <hello@nicholasly.com>
2024-11-13 16:48:25 -0600

    feat: posts with invalid author is no longer readable

--- src/components/feed.tsx
+++ src/components/feed.tsx
@@ -4,10 +4,19 @@ import dayjs from "@/lib/dayjs";
 import { getPosts } from "@/server/actions";
 
 function Post(post: Awaited<ReturnType<typeof getPosts>>[number]) {
+  if (!post.author) {
+    return (
+      <article className="rounded-lg border bg-muted p-3">
+        <p className="text-muted-foreground">
+          This post is no longer available.
+        </p>
+      </article>
+    );
+  }
+
   return (
     <article className="flex flex-col gap-3 rounded-lg border p-3">
       <div className="flex items-center gap-3">
-        {post.author ? (
           <div className="relative size-12 overflow-hidden rounded-full">
             <Image
               src={post.author.image}
@@ -16,17 +25,10 @@ function Post(post: Awaited<ReturnType<typeof getPosts>>[number]) {
               height={48}
             />
           </div>
-        ) : (
-          <div className="size-12 rounded-full bg-muted" />
-        )}
         <div className="flex flex-col">
-          <p className="font-semibold">
-            {post.author ? post.author.name : "Anonymous"}
-          </p>
+          <p className="font-semibold">{post.author.name}</p>
           <p className="text-sm text-muted-foreground">
-            <span className="font-medium">
-              @{post.author ? post.author.username : "anon"}
-            </span>
+            <span className="font-medium">@{post.author.username}</span>
             &nbsp;•&nbsp;
             <span>{dayjs(post.createdAt).fromNow()}</span>
           </p>
